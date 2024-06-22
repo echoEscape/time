24bd2da3642377e249058155909a70f2c6887a3f
echoEscape <thenumberiszero@gmail.com>
2024-06-22 22:25:54 +0200

    Expand navigation, add wireframe component

---
+++ src/app/layout/wireframe/wireframe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wireframe',
+  standalone: true,
+  imports: [],
+  templateUrl: './wireframe.component.html',
+  styleUrl: './wireframe.component.css'
+})
+export class WireframeComponent {
+
+}
